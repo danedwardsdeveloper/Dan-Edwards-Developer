901e4e8ff1c95175ecb8dc468ead6afd5b0a685a
Dan Edwards <danedwardscreative@gmail.com>
2024-05-23 17:49:21 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// function handlePrintLink() {
+//   const printLink = document.querySelector("#print-link");
+//   printLink.addEventListener("click", function () {
+//     const body = document.querySelector("body");
+//     body.style.backgroundColor = "white";
+//     console.log(body.style.backgroundColor);
+//     window.print();
+//   });
+// }
+
+// handlePrintLink();
